5a2e36983800242d579e8730cdce71b8356f2e4e
Chinakrit Lorpiyanon <Chinakrit Lorpiyanon>
2016-10-27 16:44:45 +0700

    Accepted UVa10054 - The Necklace

--- 10054.cpp
+++ 10054.cpp
@@ -1,55 +1,53 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
-int n,node,src,tar;
-int path[1001];
-int graph[50][50];
-
-bool dfs(int v,int num)
+typedef pair<int,bool> edge;
+int n,node,path[1001],cnt;
+int weight[50][50];
+void dfs(int u)
 {
-	if(num-1 == n)
+	for(int v = 0;v < node;v++) if(weight[u][v])
 	{
-		if(v != tar)	return false;
-		for(int i = 0;i < n;i++)
-			printf("%d %d\n",path[i]+1,path[i+1]+1);
-		return true;
+		weight[u][v]--;
+		weight[v][u]--;
+		dfs(v);
 	}
-	for(int i = 0;i < node;i++) if(graph[v][i])
-	{
-		graph[v][i]--;
-		if(v != i)	graph[i][v]--;
-		path[num] = i;
-		if(dfs(i,num+1))	return true;
-		graph[v][i]++;
-		if(v != i)	graph[i][v]++;
-	}
-	return false;
+	path[cnt++] = u;
 }
 
 int main()
 {
-	freopen("in.txt","r",stdin);
-	int TC,u,v;
+//	freopen("in.txt","r",stdin);
+	int TC,u,v,src;
+	bool is_euler;
 	scanf("%d",&TC);
 	for(int tc = 0;tc < TC;tc++)
 	{
 		scanf("%d",&n);
 		node = 0;
-		memset(graph,0,sizeof(graph));
+		is_euler = true;
+		memset(path,0,sizeof(path));
+		memset(weight,0,sizeof(weight));
 		for(int i = 0;i < n;i++)
 		{
 			scanf("%d %d",&u,&v);
 			node = max(node,max(u,v));
 			u--; v--;
-			if(i == 0)	src = tar = u;
-			graph[u][v]++;
-			if(u != v)	graph[v][u]++;
+			if(i == 0)	src = u;
+			path[u]++; path[v]++;
+			weight[u][v]++;	weight[v][u]++;
 		}
 		if(tc)	printf("\n");
 		printf("Case #%d\n",tc+1);
-		path[0] = src;
-		if(!dfs(src,1))	printf("some beads may be lost\n");
+		for(int i = 0;i < node && is_euler;i++)
+			is_euler = !(path[i] & 1);
+		if(!is_euler)	printf("some beads may be lost\n");
+		else
+		{
+			cnt = 0;
+			dfs(src);
+			for(int i = 1;i < cnt;i++)	printf("%d %d\n",path[i] + 1,path[(i + 1) % n] + 1);
+		}
 	}
 
 	return 0;
