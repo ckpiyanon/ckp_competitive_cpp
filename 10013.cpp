4832bc57bb040f1dfbac04bb5ef4b3b5e623a9e3
Chinakrit Lorpiyanon <Chinakrit Lorpiyanon>
2016-12-21 12:48:04 +0700

    Optimised UVa10013 - Super long sums

--- 10013.cpp
+++ 10013.cpp
@@ -8,20 +8,22 @@ int main()
 {
 //	freopen("in.txt","r",stdin);
 //	freopen("out.txt","w",stdout);
-	int TC,n,a,b;
+	int TC,n;
+	char a,b;
 	scanf("%d",&TC);
 	while(TC--)
 	{
 		scanf("%d",&n);
 		for(int i = 0;i < n;i++)
 		{
-			scanf("%d %d",&a,&b);
-			ans[i] = a + b;
+			scanf(" %c %c",&a,&b);
+			ans[i] = a - '0' + b - '0';
 		}
 		for(int i = n-1;i >= 0;i--)
 			if(ans[i] >= 10 && i > 0)
 				ans[i] -= 10,ans[i-1]++;
-		for(int i = 0;i < n;i++)	printf("%d",ans[i]);
+		if(ans[0] >= 10)	putchar('1'),ans[0] -= 10;
+		for(int i = 0;i < n;i++)	putchar(ans[i] + '0');
 		puts(TC ? "\n":"");
 	}
 
