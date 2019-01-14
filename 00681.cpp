5a94dd3113d1f3655aa1b320e8493f52eaa75b86
Chinakrit Lorpiyanon <chinakrit@orcsoft.co.th>
2019-01-14 17:58:39 +0700

    working on UVa00681 - Convex Hull Finding

--- 00681.cpp
+++ 00681.cpp
@@ -26,17 +26,30 @@ int main()
 			scanf("%d %d",&x,&y);
 			arr.push_back(ii(x,y));
 		}
-		arr.pop_back();
+		if(arr[arr.size() - 1] == arr[0])
+            arr.pop_back();
 		scanf("%d",&x);
 		idx = 0;
 		for(int i = 1;i < arr.size();i++)
 			if(arr[i].Y < arr[idx].Y || arr[i].Y == arr[idx].Y && arr[i].X < arr[idx].X)
 				idx = i;
+		swap(arr[0],arr[idx]);
+		ii tp = arr[0];
+		sort(arr.begin() + 1,arr.end(),[&tp](const ii &a,const ii &b) -> bool {
+			return atan((double)(a.Y - tp.Y) / (a.X - tp.X)) < atan((double)(b.Y - tp.Y) / (b.X - tp.X));
+		});
 		convex.clear();
 		for(int i = 0;i <= arr.size();i++)
 		{
-			ii p = arr[(idx + i) % arr.size()];
-			while(convex.size() >= 2 && direction(convex[convex.size() - 2],convex[convex.size() - 1],p) < 0)
+//			ii p = arr[(idx + i) % arr.size()];
+			ii p = arr[i % arr.size()];
+//			printf("------\n");
+//			for(int i = 0;i < convex.size();i++)
+//			{
+//				printf("%d %d\n",convex[i].X,convex[i].Y);
+//			}
+//			printf("------\n");
+			while(convex.size() > 2 && direction(convex[convex.size() - 2],convex[convex.size() - 1],p) <= 0)
 				convex.pop_back();
 			convex.push_back(p);
 		}
--- test.cpp
+++ test.cpp
@@ -3,19 +3,7 @@
 using namespace std;
 int main()
 {
-	bool arr[SIZE];
-	for(int i = 0;i < SIZE;i++)
-		arr[i] = false;
-	for(int i = 0;i < SIZE / 2;i++)
-	{
-		int pos = 1;
-		while(pos < SIZE / 2)
-		{
-			arr[pos] = !arr[pos];
-			pos = pos * 2 + !arr[pos];
-		}
-		printf("%d\n",pos);
-	}
+    printf("%lf\n",0.0 / 1);
 
 	return 0;
 }
